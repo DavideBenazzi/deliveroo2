991117b6ad7649c9c97c665bd80a204c7ec2b9bb
Davide Benazzi <benazzi.davide@gmail.com>
2021-02-16 12:55:48 +0100

    Primo Push

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+/**ADMIN ROUTE HOME CONTROLLER */
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function() {
+        //home admin
+        Route::get('/', 'HomeController@index')->name('home');
+
+        //rotte post CRUD
+        // Route::resource('restaurant', 'RestaurantController');
+    });
