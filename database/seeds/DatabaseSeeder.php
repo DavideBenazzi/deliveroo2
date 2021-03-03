492a5cb5f7f163caa3f84df0cfce2b3a87b8f502
StefanoSerafini <stefanoserafini7@gmail.com>
2021-03-03 17:04:02 +0100

    db seed commentato

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypesTableSeeder::class,
-            UsersTableSeeder::class,
+            //UsersTableSeeder::class,
         ]);
     }
 }
